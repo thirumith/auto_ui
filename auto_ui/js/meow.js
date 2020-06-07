6ca810650a364c89025751714b8c2b5879c46620
Thiru <dmthiruna@gmail.com>
2020-06-07 20:07:14 +0530

    adding chagnes

--- auto_ui/js/meow.js
+++ auto_ui/js/meow.js
@@ -1,18 +1,37 @@
-var obj = "";
+var urlObj = new Object();  
 function meow(ele, e) {
 	if (e.isTrigger != undefined){
-		window.open(obj[ele.value]);
+		window.open(urlObj[ele.value]);
 	}
 }
-$(function() {
+function onloadMeow(){
+	$.getJSON('json/url.json', function(data) {
+	   var size = 0;
+	   $.each(data.url, function(i, f) {
+		  
+		  for (var key in f) {
+			size += 1;
+			if (f.hasOwnProperty(key)) {           
+				urlObj[key]= f[key];
+			}
+		}
+	 });
+	 $("#urlCount").text(size);
+   });
+}
+function loadAuto() {
 	$('input.autocomplete').autocomplete({
-		data: {
-			"kibana": "http://placehold.it/250x250",
-			"kibana2": "http://placehold.it/250x250",
-		},
-		limit: 20,
+		data: urlObj,
+		limit: 10,
 	});
-});
+}
+function loadMe(){
+	onloadMeow();
+	setTimeout(function() {
+		loadAuto();
+	}, 3000);
+
+}
 $(document).on({
 	"contextmenu": function(e) {
 		e.preventDefault();
